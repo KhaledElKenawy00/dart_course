4274784f4cb344574474ea4a186bb1e648674234
Khaled_ElKenawey <KHALED.MOUSTAFA3032@el-eng.menofia.edu.eg>
2024-10-08 23:39:14 +0300

    all operators in dart

--- lib/introduction.dart
+++ lib/introduction.dart
@@ -1,39 +1,83 @@
 void main() {
-  void main() {
-    // تعريف رقمين
-    int num1 = 10;
-    int num2 = 3;
+  // // تعريف رقمين
+  int num1 = 10;
+  int num2 = 3;
 
-    // القيام بالحسابات الحسابية
-    int sum = num1 + num2; // الجمع
-    int diff = num1 - num2; // الطرح
-    int unaryMinus = -num1; // الناقص الأحادي
-    int mul = num1 * num2; // الضرب
-    double div = num1 / num2; // القسمة
-    int div2 = num1 ~/ num2; // القسمة الصحيحة
-    int mod = num1 % num2; // الباقي
+  // // القيام بالحسابات الحسابية
+  int sum = num1 + num2; // 13 الجمع
+  int diff = num1 - num2; // 7الطرح
+  int unaryMinus = -num1; //-10 الناقص الأحادي
+  int mul = num1 * num2; //30 الضرب
+  double div = num1 / num2; // 3.333القسمة
+  int div2 = num1 ~/ num2; // 3القسمة الصحيحة
+  int mod = num1 % num2; // 1الباقي
 
-    // طباعة النتائج
-    print("الجمع هو $sum.");
-    print("الطرح هو $diff.");
-    print("الناقص الأحادي هو $unaryMinus.");
-    print("الضرب هو $mul.");
-    print("القسمة هي $div.");
-    print("القسمة الصحيحة هي $div2.");
-    print("الباقي هو $mod.");
-  }
+  // // طباعة النتائج
+  print("الجمع هو $sum.");
+  print("الطرح هو $diff.");
+  print("الناقص الأحادي هو $unaryMinus.");
+  print("الضرب هو $mul.");
+  print("القسمة هي $div.");
+  print("القسمة الصحيحة هي $div2.");
+  print("الباقي هو $mod.");
 
-// Output:
-// الجمع هو 13.
-// الطرح هو 7.
-// الناقص الأحادي هو -10.
-// الضرب هو 30.
-// القسمة هي 3.3333333333333335.
-// القسمة الصحيحة هي 3.
-// الباقي هو 1.
-//operator
-//plus
-//minus
-//devide
-//multyply
+//عوامل الزياده والنقصان
+  int count = 5;
+
+  // زيادة العداد بواحد
+  count++;
+  print(count); // Output: 6
+
+  // نقص العداد بواحد
+  count--;
+  print(count);
+
+  // عوامل الاسناد
+  int num = 10;
+  print(num); // Output: 10
+
+  num = 5;
+  print(num);
+
+  // // عوامل الاسناد  المركبة
+
+  num1 += num2; // num1 = num1 + num2;
+  print(num1); // Output: 15
+
+  num1 -= num2; // num1 = num1 - num2;
+  print(num1); // Output: 10
+
+  num1 *= num2; // num1 = num1 * num2;
+  print(num1); // Output: 50
+
+  num1 ~/= num2; // num1 = num1 / num2;
+  print(num1);
+
+  // عوامل المقارنة
+
+  bool isEqual = num1 == num2; // falseيساوي
+  bool isNotEqual = num1 != num2; //true لا يساوي
+  bool isGreater = num1 > num2; //true أكبر من
+  bool isLess = num1 < num2; // أقل منfalse
+  bool isGreaterOrEqual = num1 >= num2; //true أكبر من أو يساوي
+  bool isLessOrEqual = num1 <= num2; // falseأقل من أو يساوي
+
+  print(isEqual); // Output: false
+  print(isNotEqual); // Output: true
+  print(isGreater); // Output: false
+  print(isLess); // Output: true
+  print(isGreaterOrEqual); // Output: false
+  print(isLessOrEqual);
+
+// العوامل المنطقية
+  bool isTrue = true;
+  bool isFalse = false;
+
+  bool logicalAnd = isTrue && isFalse; //false و
+  bool logicalOr = isTrue || isFalse; //true أو
+  bool logicalNot = !isTrue; //false ليس
+
+  print(logicalAnd); // Output: false
+  print(logicalOr); // Output: true
+  print(logicalNot);
 }
